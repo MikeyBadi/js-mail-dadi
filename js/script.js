fa7811cd9cd2dbc8c0b10903988c094e82a6d6fa
MikeyBadi <mihaibadia.1999@gmail.com>
2022-04-11 19:59:25 +0200

    Exercise finished

--- js/script.js
+++ js/script.js
@@ -5,27 +5,47 @@
 
 
 // array email
-const email = ["mihai@gmail.com", "domenico@gmail.com", "francesco@gmail.com", "ivan@gmail.com"]
+const email = ["mihai@gmail.com", "domenico@gmail.com", "francesco@gmail.com", "ivan@gmail.com"];
 
 // mail inserita
-const userMail = prompt('Inserisci di seguito la tua mail')
-
+const userMail = prompt('Inserisci di seguito la tua mail');
 let convalidEmail = false
 
+const diceStart = document.querySelector("#button_dice");
+
 for (let i=0; i < email.length; i++) {
-  console.log(i)
+  console.log(i,email[i],userMail);
   if (email[i] === userMail){
     convalidEmail = true;
-    const output= document.getElementById("output_text")
-    document.getElementById("output_text").innerHTML ="Loggato correttamente"
-  } else {
-    document.getElementById("output_text").innerHTML ="Registrati per poter accedere"
   }
+} 
+
+if (convalidEmail){
+  const output= document.getElementById("output_text")
+  document.getElementById("output_text").innerHTML ="Loggato correttamente";
+} else {
+  document.getElementById("output_text").innerHTML ="Registrati per poter accedere";
+  diceStart.style.cssText="display: none;"; 
 }
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+console.log(diceStart)
+diceStart.addEventListener('click', function(){
+  const player = Math.ceil(Math.random()*6)
+  console.log('Player ',player)
 
-const diceStart = document.querySelector("#button_dice")
+  const comp =Math.ceil(Math.random()*6)
+  console.log('Computer',comp)
+
+  if(player > comp){
+    document.querySelector("#winner").innerHTML ="HAI VINTO!"
+  } else if (player == comp){
+    document.querySelector("#winner").innerHTML ="HAI PAREGGIATO!"
+  } else {
+    document.querySelector("#winner").innerHTML ="HAI PERSO!"
+  };
+
+});
